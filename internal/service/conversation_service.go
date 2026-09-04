69baee815232dbd0168cae961bca63a3606fb56c
chilljzz <q1044038561@gmail.com>
2026-09-05 00:24:23 +0800

    feat: map channels to conversations

---
+++ internal/service/conversation_service.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/chilljzz/gohub/internal/model"
+	"github.com/chilljzz/gohub/internal/repository"
+	"gorm.io/gorm"
+)
+
+type ConversationService struct {
+	conversationRepo *repository.ConversationRepository
+}
+
+var (
+	ErrConversationNotFound = errors.New("conversation not found")
+)
+
+func NewConversationService(
+	conversationRepo *repository.ConversationRepository,
+) *ConversationService {
+	return &ConversationService{
+		conversationRepo: conversationRepo,
+	}
+}
+
+func (s *ConversationService) GetChannelConversation(
+	channelID uint,
+) (*model.Conversation, error) {
+	conversation, err := s.conversationRepo.FindByChannelID(channelID)
+
+	if err != nil {
+		return nil, err
+	}
+	if conversation == nil {
+		return nil, ErrConversationNotFound
+	}
+
+	return conversation, nil
+
+}
+
+func (s *ConversationService) EnsureChannelConversation(
+	channelID uint,
+) (*model.Conversation, error) {
+	existing, err := s.conversationRepo.FindByChannelID(channelID)
+	if err != nil {
+		return nil, err
+	}
+
+	if existing != nil {
+		return existing, nil
+	}
+
+	conversation := &model.Conversation{
+		Type:      model.ConversationTypeChannel,
+		ChannelID: &channelID,
+	}
+
+	err = s.conversationRepo.Create(conversation)
+
+	if err == nil {
+		return conversation, nil
+	}
+
+	if !errors.Is(err, gorm.ErrDuplicatedKey) {
+		return nil, err
+	}
+
+	return s.conversationRepo.FindByChannelID(channelID)
+
+}
